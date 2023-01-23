74d5dccb15e5d7b43bf66c0f5e4bcf0675888e96
Anel <anelyamazhit@mail.ru>
2023-01-24 02:18:58 +0600

    anel

--- app/src/main/java/com/example/tuimedaq/LoginActivity.kt
+++ app/src/main/java/com/example/tuimedaq/LoginActivity.kt
@@ -1,9 +1,15 @@
 package com.example.tuimedaq
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.example.tuimedaq.databinding.ActivityLoginBinding
+import com.jakewharton.rxbinding2.widget.RxTextView
+import io.reactivex.Observable
+
+@SuppressLint("CheckResult")
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -11,11 +17,56 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val usernameStream = RxTextView.textChanges(binding.etEmail)
+            .skipInitialValue()
+            .map { username ->
+                username.isEmpty()
+            }
+
+        usernameStream.subscribe {
+            showTextMinimalAlert(it, "Email/Username")
+        }
+
+        val passwordStream = RxTextView.textChanges(binding.etPassword)
+            .skipInitialValue()
+            .map { password ->
+                password.isEmpty()
+            }
+        passwordStream.subscribe {
+            showTextMinimalAlert(it, "Password")
+        }
+
+        val invalidFieldsStream = Observable.combineLatest (
+            usernameStream,
+            passwordStream
+        ) { usernameInvalid: Boolean, passwordInvalid: Boolean ->
+            !usernameInvalid && !passwordInvalid
+        }
+        invalidFieldsStream.subscribe { isValid ->
+            if (isValid) {
+                binding.btnLogin.isEnabled = true
+                binding.btnLogin.backgroundTintList =
+                    ContextCompat.getColorStateList(this, R.color.pink)
+            } else {
+                binding.btnLogin.isEnabled = false
+                binding.btnLogin.backgroundTintList =
+                    ContextCompat.getColorStateList(this, android.R.color.darker_gray)
+            }
+        }
+
         binding.btnLogin.setOnClickListener {
             startActivity(Intent(this, HomeActivity::class.java))
         }
         binding.tvHaventAccount.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
         }
+    }
+
+    private fun showTextMinimalAlert(isNotValid: Boolean, text: String) {
+        if (text == "Email/Username")
+            binding.etEmail.error = if (isNotValid) "$text should contain at least 6 digits" else null
+        else if (text == "Password")
+            binding.etPassword.error = if (isNotValid) "$text contain at least 8 digits!" else null
     }
 }
